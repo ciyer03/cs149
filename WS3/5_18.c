ef88223c11e7685af8fe30db9771b326092796b3
safiullah <safisaif4444@gmail.com>
2023-09-18 23:00:00 -0700

    Made changes to the 5_18.c file

--- WS3/5_18.c
+++ WS3/5_18.c
@@ -6,31 +6,37 @@
 */
 
 #include <stdio.h>
+#define MAX_SIZE 9
 
 //This is the main function. This is used to generate the desired output from the input given by the user.
 int main(int argc, char *argv[])
 {
-    int count = 1;
-    int num;
-    int list[9];
-    printf("Enter the list of numbers: ");
-    scanf("%d", &num);
-    list[0] = num;
-    while (count < 9 && num >= 0) {
-        scanf("%d", &num);
-        list[count] = num;
-        count++;
+    int count = 0; // Index count
+    int input = 0; // Stores the user input
+
+    int list[MAX_SIZE];
+
+    // Reads in values from the input.
+    while (input >= 0) {
+        if (count > 9) { // Don't want to read in more than 9 values.
+            printf("Too many numbers\n");
+            return 1;
+        }
+
+        // Take input from the user.
+        printf("\nEnter a number: ");
+        if ((scanf("%d", &input)) < 0) {
+            printf("\nIO Error.\n");
+            return 1;
+        }
+        list[count] = input; // Store the value in the array.
+        count++; // Increment the index.
     }
 
-    int midItem = list[count/2];
-    int midIndex = count/2 - 1;
+    // Gather the middle index.
+    int midItem = list[(count/2) - 1];
 
-    if (count > 9) {
-        printf("Too many numbers\n");
-    }
-    else {
-        printf("%d, %d,  Middle item: %d\n", midIndex, count, midItem);
-    }
+    printf("Middle item: %d\n", midItem);
 
     return 0;
 }
