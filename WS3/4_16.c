040394c15b72b5cc2b2ca6368bb749910f39a721
safiullah <safisaif4444@gmail.com>
2023-09-18 23:23:34 -0700

    Made changes to 4_16.c

--- WS3/4_16.c
+++ WS3/4_16.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     int num;
     printf("Enter the non-negative numbers: ");
     scanf("%d", &num);
-    int total = 0;
+    double total = 0;
     int max = num;
     int count = 0;
     while (num >= 0) {
