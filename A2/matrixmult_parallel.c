a9d50a6e3e4fd73b860f9643d3c0ebd24d7156bf
Chandramouli Iyer <chandramouli.iyer@sjsu.edu>
2023-09-23 23:29:19 -0700

    Added logic for calculating dot product of specific rows with another matrix.

--- A2/matrixmult_parallel.c
+++ A2/matrixmult_parallel.c
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/wait.h>
 
 #define MAX_COLUMNS 8
 #define MAX_ROWS 8
@@ -21,9 +21,10 @@
 // Function prototypes. See the function declarations for more information.
 void zeroOut(int *matrix, int rows, int columns);
 void fillMatrix(int* matrix, int rows, int columns, FILE *file);
-void dotProduct(int* matrix1, int* matrix2, int* product);
+void rowSum(int* matrix1, int* matrix2, int* product, int row);
 void printArr(int *matrix, int rows, int columns);
 void makeResult(int *matrix1, int *matrix2, int *resultant);
+void fillRow(int row, int *sourceMatrix, int *resultant);
 
 /**
  * Executes the program. Accepts command line parameters.
@@ -75,7 +76,7 @@ int main(int argc, char *argv[])
 
     int resultant[MAX_ROWS][MAX_COLUMNS]; // Matrix of size 8x8. The resultant matrix.
 
-    int fd[MAX_PROCESSES + 1][2];
+    int fd[MAX_PROCESSES][2];
 
     // Create pipes for read/write for all processes.
     for (int i = 0; i < MAX_PROCESSES; ++i) {
@@ -89,6 +90,43 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < MAX_PROCESSES; ++i) {
         pid[i] = fork();
+    }
+
+    // Parent process.
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        int wstatus;
+        int childPID = wait(&wstatus);
+        if (WIFEXITED(wstatus)) { // If the child process exited normally.
+            int exitStatus = WEXITSTATUS(wstatus);
+            if (exitStatus == 0) { // If the child process exited with code 0 (success)
+                
+                // Holds the row values returned by the child process.
+                int rowResult[MAX_COLUMNS];
+
+                int rowCompleted; // The row that the child process calculated.
+
+                // Reads in the row that the row that the child process calculated.
+                if (read(fd[MAX_PROCESSES][0], &rowCompleted, sizeof(int)) == -1) {
+                    printf("Error while reading value. Problematic child: %d\n", 
+                           childPID);
+                    return 1;
+                }
+
+                // Reads in the row values that the child process calculated.
+                if (read(fd[MAX_PROCESSES][0], &rowResult, sizeof(int) * MAX_COLUMNS)
+                    == -1) {
+                    printf("Error while reading value. Problematic child: %d\n", 
+                           childPID);
+                    return 1;
+                }
+                
+                fillRow(rowCompleted, rowResult, &(resultant[0][0]));
+
+            } else {
+                printf("Child %d exited abnormally with code %d.\n", childPID, 
+                       exitStatus);
+            }
+        }
     }
     
     printf("Result of %s*%s is = ", argv[1], argv[2]);
@@ -160,7 +198,7 @@ void fillMatrix(int* matrix, int rows, int columns, FILE *file) {
     int *preOp = matrix; // Store the memory address of matrix prior to manipulation.
     int index = 0; // Tracks how far into valid memory we are in.
 
-    char line[60]; // Assuming we won't read in more than 60 characters at a time.
+    char line[100]; // Assuming we won't read in more than 100 characters at a time.
 
     // Index must be less than product for the program to be in valid memory range for 
     // modification. Also check if we haven't already reached EOF.
@@ -206,50 +244,52 @@ void fillMatrix(int* matrix, int rows, int columns, FILE *file) {
 }
 
 /**
- * Calculates the dot product of two given matrices and stores the result in another
- * provided matrix. Look at the header comments of the zeroOut() function to understand 
- * why pointers are being passed instead of an array.
+ * Calculates the dot product of the specified row in matrix1 with the entirety of 
+ * matrix2, and stores the result in the product result.
  *
- * Assumption: matrix1 has a size of 1x3, and matrix2 has a size of 3x5. All matrices 
- * must be provided as arguments, including the one in which the result will be stored.
+ * Assumption: matrix1 and matrix 2 have a size of 8x8, and the product matrix has a 
+ * size of 1x8.
  *
- * Input parameters: matrix1: Matrix with the size 1x3.
- *                   matrix2: Matrix with the size 3x5.
+ * Input parameters: matrix1: Matrix with the size 8x8.
+ *                   matrix2: Matrix with the size 8x8.
  *                   product: Matrix in which the dot product will be stored. Must be of
- *                   size 1x5.
+ *                   size 1x8.
  *
  * Returns: Nothing.
-**/
-void dotProduct(int* matrix1, int* matrix2, int* product) {
-    const int maxRowIncrement = 3; // Max amount of rows to process.
-
-    // Max amount of columns to process along with increments for each iteration of 
-    // the array pointer.
-    const int weightsIncrementAndHighestColumns = 5; 
+**/ 
+void rowSum(int* matrix1, int* matrix2, int* product, int row) {
+    // Calculates the row to be multiplied.
+    int rowStart = (row - 1 ) * MAX_ROWS;
 
     // Loop calculates the dot product of matrix1 and matrix2, and stores the result 
     // in product.
-    for (int i = 0; i < weightsIncrementAndHighestColumns; ++i) {
+    for (int i = 0; i < MAX_COLUMNS; ++i) {
         // Stores the result of multiplication of the entire row and column.
         int result = 0;
 
         // Loop calculates single row and single column multiplication.
-        for (int j = 0; j < maxRowIncrement; ++j) {
+        for (int j = 0; j < MAX_ROWS; ++j) {
 
-            /** Multiplies one row and one column and stores the output in result.
-             *  matrix1 + j increments the pointer address to point to the next value.
-            *  By dereferencing that address, we get the raw value. For the second value
-            *  we get the correct address for dereferencing by multiplying the row 
-            *  increment, j, with the constant weight increment of 5 (calculated 
-            *  according to column width), so that we always land at the appropriate 
-            *  next value in the column. Then we add the column increment, i, to the 
-            *  multiplied value to shift to the appropriate column for multiplication.
-            *  Lastly, after we get the correct pointer offset, we add that to the 
-            *  pointer address, and dereference it to get the correct raw value.
-            **/
-            result += ((*(matrix1 + j)) * 
-                (*(matrix2 + ((j * weightsIncrementAndHighestColumns) + i))));
+            /** 
+             * Multiplies one row and one column and stores the output in result.
+             * For the first value, we add the row increment with rowStart to both, get
+             * to the appropriate row for multiplication and to the corresponding row 
+             * position. By dereferencing that address, we get the raw value.
+             *
+             * For the second value we get the correct address for dereferencing by 
+             * multiplying the row increment, j, with the constant weight increment of
+             * 8 (calculated according to column width), so that we always land at the 
+             * appropriate next value in the column. Then we add the column increment,
+             * i, to the multiplied value to shift to the appropriate column for 
+             * multiplication. Lastly, after we get the correct pointer offset, we add 
+             * that to the pointer address, and dereference it to get the correct raw 
+             * value.
+             **/
+            int rowIncrement = rowStart + j;
+            int columnIncrement = (j * MAX_COLUMNS) + i;
+            result += (*(matrix1 + rowIncrement)) * (*(matrix2 + columnIncrement));
         }
+
         /** Store the calculated result in the appropriate dereferenced column 
           * (offset calculated with the pointer address plus column increment, i) in the
           * product array.
@@ -257,6 +297,15 @@ void dotProduct(int* matrix1, int* matrix2, int* product) {
         *(product + i) = result;
     }
 }
+
+/**
+ *
+ *
+ **/
+void fillRow(int row, int *sourceMatrix, int *resultant) {
+
+}
+
 
 /**
  * Prints the contents of the given matrix. Since we are using pointers, we calculate 
